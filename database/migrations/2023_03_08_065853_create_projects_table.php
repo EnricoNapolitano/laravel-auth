0b3c8d911168123110c99c7efa8790b6583d45db
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 08:35:04 +0100

    setted columns for projects table

--- database/migrations/2023_03_08_065853_create_projects_table.php
+++ database/migrations/2023_03_08_065853_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
