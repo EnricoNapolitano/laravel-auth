0fcebe1d3603f6ebd8bd302af16a981b68b718d2
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 18:18:34 +0100

    added route to create in project index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,31 +1,34 @@
 @extends('layouts.app')
 @section('content')
+    <div class="container">
+        <table class="container table table-hover mt-5">
+            <thead>
+                <th>#id</th>
+                <th>Title</th>
+                <th>Created at</th>
+                <th>Updated at</th>
+                <th>Actions</th>
+            </thead>
+            <tbody>
+                @forelse($projects as $project)
+                <tr>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->created_at }}</td>
+                    <td>{{ $project->updated_at }}</td>
+                    <td>
+                        <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td class="text-center" colspan="5">There aren't projects yet.</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+        <a href="{{ route('admin.projects.create') }}" class="btn btn-primary"><i class="fa-solid fa-upload me-2"></i>Upload Project</a>
+    </div>
 
-    <table class="container table table-hover mt-5">
-        <thead>
-            <th>#id</th>
-            <th>Title</th>
-            <th>Created at</th>
-            <th>Updated at</th>
-            <th>Actions</th>
-        </thead>
-        <tbody>
-            @forelse($projects as $project)
-            <tr>
-                <td>{{ $project->id }}</td>
-                <td>{{ $project->title }}</td>
-                <td>{{ $project->created_at }}</td>
-                <td>{{ $project->updated_at }}</td>
-                <td>
-                    <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
-                </td>
-            </tr>
-            @empty
-            <tr>
-                <td class="text-center" colspan="5">There aren't projects yet.</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
 
 @endsection
