3155319eff39eae1ac96bc5fad954373263e8a43
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 08:22:04 +0100

    setted routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\HomeController as GuestHomeController;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [GuestHomeController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function (){
+    Route::get('/', [AdminHomeController::class, 'index'])->name('home');
+
+    /* #CRUD for 'Project' -----------------------------------------------------
+    Route::get('/Projects', [ProjectController::class, 'index'])->('projects.index');
+    Route::get('/Projects/{project}', [ProjectController::class, 'show'])->('projects.show');
+    Route::get('/Projects/create', [ProjectController::class, 'create'])->('projects.create');
+    Route::post('/Projects', [ProjectController::class, 'store'])->('projects.store');
+    Route::get('/Projects/{project}/edit', [ProjectController::class, 'edit'])->('projects.edit');
+    Route::get('/Projects/{project}/edit', [ProjectController::class, 'edit'])->('projects.edit');
+    Route::put('/Projects/{project}', [ProjectController::class, 'update'])->('projects.update');
+    Route::delete('/Projects/{project}', [ProjectController::class, 'destroy'])->('projects.destroy'); */
+
+    // Code below groups all the CRUD's routes
+    Route::resource('posts', ProjectController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
