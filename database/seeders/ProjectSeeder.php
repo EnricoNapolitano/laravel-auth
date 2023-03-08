01133fc87d31da619612d65f180b8360aaad5943
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 08:50:07 +0100

    seeding projects table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i=0; i<10; $i++){
+            $project = new Project();
+            $project->title = $faker->text(50);
+            $project->content = $faker->paragraphs(30, true);
+            $project->image = $faker->imageUrl(960, 540);
+            $project->slug = $faker->Str::slug($project->title, '-');
+            $project->save();
+        }
     }
 }
