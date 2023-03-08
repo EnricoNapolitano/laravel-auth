51c99792e20e29236ce1f44cb2d6e72655c8b2f0
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 22:20:20 +0100

    showing errors about validation

--- resources/views/includes/admin/form.blade.php
+++ resources/views/includes/admin/form.blade.php
@@ -1,5 +1,7 @@
 <main class="container mt-5">
 
+    @include('includes.alerts.error')
+
     @if($project->exists)
     <form action="{{ route('admin.projects.update', $project->id) }}" method="POST" novalidate>
     @method('PUT')
@@ -9,26 +11,17 @@
     @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Project Title</label>
-            <input type="text" class="form-control" id="title" placeholder="Write a title for your project" name="title" value="{{ old('title',$project->title) }}" required>
+            <input type="text" class="form-control" id="title" placeholder="Write a title for your project" name="title" value="{{ old('title', $project->title) }}" minlength="5" maxlength="50" required>
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Project Image Url</label>
-            <input type="url" class="form-control" id="image" placeholder="Insert url image..." name="image" value="{{ old('image',$project->image) }}" required>
+            <input type="url" class="form-control" id="image" placeholder="Insert url image..." name="image" value="{{ old('image', $project->image) }}" required>
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Project Content</label>
-            <textarea class="form-control" id="content" rows="3" name="content" placeholder="Describe your project..." value="{{ old('content',$project->content) }}"  required></textarea>
+            <textarea class="form-control" id="content" rows="3" name="content" placeholder="Describe your project..." value="{{ old('content', $project->content) }}"  required></textarea>
         </div>
-        <div class="d-flex">
             <button type="submit" class="btn btn-primary"><i class="fa-solid fa-upload me-2"></i>{{ $project->exists ? 'Update' : 'Upload'}}</button>
-            @if($project->exists)
-            <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" class="container">
-            @csrf
-            @method('DELETE')
-                <button class="btn btn-warning ms-2">Delete</button>
-            </form>
-            @endif
-        </div>
     </form>
 
 </main>
