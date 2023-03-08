83c2d84369d88ea96ef46e02e0e3373e556be231
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 19:08:49 +0100

    created include admin form

---
+++ resources/views/includes/admin/form.blade.php
@@ -0,0 +1,25 @@
+<main class="container mt-5">
+
+    @if($project->exists)
+    <form action="{{ route('admin.projects.update', $project->id) }}" method="POST" novalidate>
+    @method('PUT')
+    @else
+    <form action="{{ route('admin.projects.store') }}" method="POST" novalidate>
+    @endif
+    @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Project Title</label>
+            <input type="text" class="form-control" id="title" placeholder="Write a title for your project" name="title" value="{{ old('title',$project->title) }}" required>
+        </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Project Image Url</label>
+            <input type="url" class="form-control" id="image" placeholder="Insert url image..." name="image" value="{{ old('image',$project->image) }}" required>
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Project Content</label>
+            <textarea class="form-control" id="content" rows="3" name="content" placeholder="Describe your project..." value="{{ old('content',$project->content) }}"  required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary"><i class="fa-solid fa-upload me-2"></i>{{ $project->exists ? 'Update' : 'Upload'}}</button>
+    </form>
+
+</main>
