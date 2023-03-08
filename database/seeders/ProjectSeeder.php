fb352a310bbbef7237b084a351dc5a420cc98bef
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 09:08:50 +0100

    fixing typos in ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use App\Models\Project;
 use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
@@ -14,12 +15,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for($i=0; i<10; $i++){
+        for($i=0; $i<10; $i++){
             $project = new Project();
             $project->title = $faker->text(50);
             $project->content = $faker->paragraphs(30, true);
             $project->image = $faker->imageUrl(960, 540);
-            $project->slug = $faker->Str::slug($project->title, '-');
+            $project->slug = Str::slug($project->title, '-');
             $project->save();
         }
     }
