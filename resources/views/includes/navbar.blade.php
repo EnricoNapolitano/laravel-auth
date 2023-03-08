25b45e0568a4c6b5f7cfbcd2f951b3faec59fe68
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 22:34:06 +0100

    navbar: auth and guest conditions

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -3,6 +3,7 @@
     <a class="navbar-brand" href="#">Portfolio</a>
     
     <div class="collapse navbar-collapse" id="navbarScroll">
+    @auth
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="{{ route('admin.home') }}">Home</a>
@@ -11,7 +12,10 @@
           <a class="nav-link" href="{{ route('admin.projects.index') }}">Projects</a>
         </li>
       </ul>
+    @endauth
+    @guest
       <a class="nav-link" href="{{ route('admin.home') }}">Login</a>
+    @endguest
     </div>
   </div>
 </nav>
