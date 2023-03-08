f03135c215dc823a010d02c0aaca31c6ebeb48ba
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 14:26:41 +0100

    created navbar content

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="container navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Portfolio</a>
+    
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+      </ul>
+      <a class="nav-link" href="{{ route('admin.home') }}">Login</a>
+    </div>
+  </div>
+</nav>
