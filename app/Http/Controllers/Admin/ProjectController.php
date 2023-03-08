a3bfdb2cc81937dd346ad46d4260b9711172a3f0
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-08 14:59:16 +0100

    added project index controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::orderBy('id', 'ASC')->get();
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
